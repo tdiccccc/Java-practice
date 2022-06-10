4bec4d09123682f5af10d8d9c1184aee57bb76c5
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-06-10 11:49:59 +0900

    [finish]4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "田口大地";
+		System.out.println(name);
+
+		name = "aaa";
+		System.out.println(name);
+
+		final int NUMBER = 1;
+		System.out.println(NUMBER);
+	}
+}
