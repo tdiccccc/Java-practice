ca2b86df613ed82b4114270a072310deeb14e63f
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-06-10 12:36:41 +0900

    [finish]5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,33 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 1000000000000L;
+		System.out.println(l);
+
+		double d = 163.1;
+		System.out.println(d);
+
+		boolean b = false;
+		System.out.println(b);
+
+		Float float1 = 163.1F;
+		System.out.println(float1);
+
+		Integer integer1 = 5;
+		System.out.println(integer1);
+
+		Character char1 = 'a';
+		System.out.println(char1);
+
+		int[] array1 = new int[5];
+		array1[0] = 10;
+		array1[1] = 20;
+		array1[2] = 30;
+		array1[3] = 40;
+		array1[4] = 50;
+
+		int int2 = array1[3];
+
+		System.out.println(int2);
+	}
+}
