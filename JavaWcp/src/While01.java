af1506f3b29cd71243157e192dd2e0444f03bd97
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-06-17 13:32:53 +0900

    [doWhile]

--- JavaWcp/src/While01.java
+++ JavaWcp/src/While01.java
@@ -1,7 +1,7 @@
 
 public class While01 {
 	public static void main(String[] args) {
-		int number = 1;
+		int number = 100;
 		while(number < 50) {
 			number *= 2;
 			System.out.println("While01 = " + number);
