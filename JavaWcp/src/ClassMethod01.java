86c06e2f67e7e638bdd207857ab87e61b04f4f3e
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-06-27 14:01:09 +0900

    [Add]実行

--- JavaWcp/src/ClassMethod01.java
+++ JavaWcp/src/ClassMethod01.java
@@ -1,4 +1,10 @@
 
 public class ClassMethod01 {
-
+	public static void main(String[] args) {
+		Human01 tamaki = new Human01();//Human01クラスのインスタンスを生成
+		System.out.println("名前は" + tamaki.name + "で、年齢は" + tamaki.age + "です。");
+		
+		Human01 muto = new Human01("武藤", 25);//Human01クラスのインスタンスを生成
+		System.out.println("名前は" + muto.name + "で、年齢は" + muto.age + "です。");
+	}
 }
