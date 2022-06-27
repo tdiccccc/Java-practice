ce3e6ca5701bcb44e4e0ebdfeebad122f12c3946
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-06-27 13:54:27 +0900

    [Add] this変数

---
+++ JavaWcp/src/Human01.java
@@ -0,0 +1,17 @@
+
+public class Human01 {
+	public String name;//メンバー変数
+	public int age;//メンバー変数
+	
+	public Human01() {
+		name = "玉木";
+		age = 20;
+	}
+	
+	//引数2つのコンストラクタ
+	
+	public Human01(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
